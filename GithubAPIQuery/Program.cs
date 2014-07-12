cbcc342dba709cc69bcc4fa8bd8ebb85083107d5
seankearon <sean@tackaberry.org>
2014-07-12 16:47:14 +0100

    Now searches when run!

--- GithubAPIQuery/Program.cs
+++ GithubAPIQuery/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace GithubAPIQuery
+﻿using System;
+
+namespace GithubAPIQuery
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Beginning search of Github repository API for 'met'.");
+
+            var searcher = new RepositorySearch();
+            var results = searcher.SearchFor("met");
+
+            foreach (var repository in results)
+            {
+                Console.WriteLine("    {0} - {1}", repository.Name, repository.Description);
+            }
+
+            Console.WriteLine("Finished - found {0} repositories for the term 'met'.", results.Length);
+            Console.Read();
         }
     }
 }
