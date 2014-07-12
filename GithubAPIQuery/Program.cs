7d41a356d167fa34afe213d251956ddf129a129f
seankearon <sean@tackaberry.org>
2014-07-12 17:26:15 +0100

    Changed search term from 'met' to 'raven'.

--- GithubAPIQuery/Program.cs
+++ GithubAPIQuery/Program.cs
@@ -6,17 +6,17 @@ namespace GithubAPIQuery
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Beginning search of Github repository API for 'met'.");
+            Console.WriteLine("Beginning search of Github repository API for 'raven'.");
 
             var searcher = new RepositorySearch();
-            var results = searcher.SearchFor("met");
+            var results = searcher.SearchFor("raven");
 
             foreach (var repository in results)
             {
                 Console.WriteLine("    {0} - {1}", repository.Name, repository.Description);
             }
 
-            Console.WriteLine("Finished - found {0} repositories for the term 'met'.", results.Length);
+            Console.WriteLine("Finished - found {0} repositories for the term 'raven'.", results.Length);
             Console.Read();
         }
     }
